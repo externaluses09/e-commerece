7b5985eb36acc432e95451fb4300c8ba8516ac91
Arun Erram <arunerram123@gmail.com>
2025-08-07 11:29:01 -0500

    clean-test

--- New e-commerece/Backend/product/src/main/java/com/codreal/product/controller/ProductController.java
+++ New e-commerece/Backend/product/src/main/java/com/codreal/product/controller/ProductController.java
@@ -36,17 +36,6 @@ public class ProductController {
     public String test(){
         return "Congratulations testing is done !";
     }
-    
-    @GetMapping("/test1")
-    public String test1(){
-        return "Congratulations testing of test1 is done ! also you are going to do great , fantastic and super !";
-    }
-
-    @GetMapping("/test2")
-    public String test2(){
-        return "Congratulations testing of test2 is done !";
-    }
-
 
 
     @PostMapping("/add")
@@ -64,5 +53,6 @@ public class ProductController {
         }
     }
 }
+
 
 
